bc40ce2cfe1ddba13ea933a0f4a3a1418b7df30a
Chris Siddall <Chris.Siddall@cpplc.com>
2020-09-21 19:12:42 +0100

    write up WIP

--- ClientApp/Components/Home/index.tsx
+++ ClientApp/Components/Home/index.tsx
@@ -3,9 +3,12 @@ import * as React from "react";
 import { IRoutedCompProps } from "../../routes";
 import { Container, Segment, Label } from "semantic-ui-react";
 import { HEADING_COLOUR } from "../../theme";
+import { Link } from "react-router-dom";
 
 interface IHomeProps extends IRoutedCompProps {
 };
+
+//sidtodo catalog of my favourite tunes.
 
 //sidtodo: create a short video?
 
@@ -54,13 +57,18 @@ const welcome: JSX.Element = (
             I trust your journey was not a stressful one.
         </p>
         <p>
-            My name is Chris Siddall, I am currently the fine age of 35 years old (1985), and are currently based in Tamworth Staffordshire.
+            My name is Chris Siddall, I am currently the fine age of 35 years old (1985) and based in Tamworth Staffordshire.
             I have had a passion for coding since discovering it at the age of 16, and have created this application as a means of demonstrating my skills, to catalog my work and experience and to give further insight into my personality.
-            It is not my intention to sound overly professional or formal because I want this to be about expressing who I am.
+            It is not my intention to sound overly professional or formal because I want to express who I am.
+        </p>
+        <p>
+            Before the COVID quarantine in March 2019 my work on this was limited to the odd spare 15 minutes I could find.
+            However being furloughed for 6 weeks gave me the wonderful opportunity to knuckle down and really put some time in.
+            It was then that I completed the majority of the <Link to="./textparse">text parse user interface</Link>, and what remained was writing everything up.
         </p>
         <h1>About Me</h1>
         <p>I am a proud father to my 6 year old son and believe this has had a very positive effect on my outlook towards life.
-            My father has helped mould me in to the person I am today so I am hoping to replicate that with my son.
+            My father has moulded me in to the person I am today so I am hoping to replicate that with my son.
             Arthur if you are reading this, your father loves you very much.
         </p>
         <p>I have always been physically active and have done many sports in my youth including kickboxing, boxing, rugby, cricket, football and even gymnastics.
@@ -71,10 +79,9 @@ const welcome: JSX.Element = (
             In my early to mid 20's I was heavily into frequenting live music events such as raves, clubbing and festivals and my favourite genre's are trance, hard house/dance, drum and bass, and UK hardcore.
             I have been to Glastonbury 4 times.
             I learned to mix my favourite categories of music using CDJ's and played out approximately 10 times at small to medium club events.
-            Those days are behind me now but I am still very passionate about music due to it's wonderful natural mood altering effects.
-            
-            
-            Nowdays I'm interested in politics .. family //sidtodo
+            Those days are behind me now but I am still very passionate about music due to it's wonderful natural stimulating effects.
+            Since becoming a father balancing my life has become even more important as I carefully manage work, family and my home as well as squeezing in time to follow my dreams.
+            Lately I'm finding myself more and more interested in politics and have been following the COVID19 crisis and American rioting very closely.
         </p>
 
         <p>I do believe there is such a thing as too much technology and think it's de-sensitising us to the world around us</p>
